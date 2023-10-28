43dcf069332836c1fd143e4ab519c777ac531f9b
Ebrahim Mansour A Sharka <ebraandomar@gmail.com>
2023-10-28 22:32:51 +0300

    The FInal Code

---
+++ ALEXNET SMALL DATASET.py
@@ -0,0 +1,98 @@
+import os
+import torch
+import random
+import numpy as np
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import ImageFolder
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models  # Import AlexNet
+
+# Set a specific random seed
+random_seed = 42
+
+# Set the random seed for Python's random module
+random.seed(random_seed)
+
+# Set the random seed for NumPy
+np.random.seed(random_seed)
+
+# Set the random seed for PyTorch
+torch.manual_seed(random_seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+# Specify the directory containing your images
+data_dir = r"C:\Users\Ebra\OneDrive\سطح المكتب\Projects\OCR-Train-and-validation-PyTorch//SmallDataset"
+
+# Define data transformations
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # Resize the input images to 224x224
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# Load your custom dataset from the directory
+custom_dataset = ImageFolder(root=data_dir, transform=transform)
+
+# Split the dataset into training and testing sets
+train_size = int(0.8 * len(custom_dataset))
+test_size = len(custom_dataset) - train_size
+train_dataset, test_dataset = random_split(custom_dataset, [train_size, test_size])
+
+# Create data loaders for training and testing
+batch_size = 32
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+# Define AlexNet and move it to the GPU
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = models.alexnet(pretrained=False, num_classes=len(custom_dataset.classes)).to(device)
+
+# Define the optimizer and criterion
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+criterion = nn.CrossEntropyLoss()
+
+# Training hyperparameters
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    model.train()
+    total_loss = 0
+    correct = 0
+    total = 0
+
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+
+    accuracy = 100 * correct / total
+    print(f"Epoch {epoch + 1}, Loss: {total_loss:.4f}, Accuracy: {accuracy:.2f}%")
+
+model.eval()
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        outputs = model(inputs)
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+
+accuracy = 100 * correct / total
+print(f"Testing Accuracy for AlexNet: {accuracy:.2f}%")
---
+++ VGG16 CIFAR.py
@@ -0,0 +1,89 @@
+import torch
+import random
+import numpy as np
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, random_split
+
+# Set the seed for PyTorch, Python's random module, and NumPy
+seed = 61
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+random.seed(seed)
+np.random.seed(seed)
+
+# Check if a GPU is available
+print(torch.cuda.device_count())
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(torch.cuda.is_available())
+print(f"Using device: {device}")
+
+# Define the dataset and data loaders (CIFAR-10 as an example)
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False, pin_memory=True)
+
+# Define VGG16
+model = models.vgg16(pretrained=False, num_classes=10)  # Update the number of output classes to 10 for CIFAR-10
+model.to(device)  # Move the model to the GPU
+
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+criterion = nn.CrossEntropyLoss()
+
+# Training hyperparameters
+num_epochs = 20
+
+for epoch in range(num_epochs):
+    model.train()
+    total_loss = 0
+    correct = 0
+    total = 0
+
+    for iteration, (inputs, labels) in enumerate(train_loader):
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        predicted = outputs.max(1)[1]
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+
+    # Calculate and print the average loss and accuracy for the epoch
+    epoch_loss = total_loss / (iteration + 1)
+    epoch_accuracy = 100 * correct / total
+    print(f"Epoch {epoch + 1} - Average Loss: {epoch_loss:.4f}, Average Accuracy: {epoch_accuracy:.2f}%")
+
+model.eval()
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        predicted = outputs.max(1)[1]
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+
+    accuracy = 100 * correct / total
+    print(f"Testing Accuracy for VGG16 on CIFAR-10: {accuracy:.2f}%")
